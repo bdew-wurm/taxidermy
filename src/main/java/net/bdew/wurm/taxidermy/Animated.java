3c99b8884f9ed0b28842ebe298e3d2300eeb8a93
bdew <blackdew@gmail.com>
2018-03-02 01:42:41 +0200

    Animated trophies will emit light if a glow rune is attached

--- src/main/java/net/bdew/wurm/taxidermy/Animated.java
+++ src/main/java/net/bdew/wurm/taxidermy/Animated.java
@@ -33,6 +33,20 @@ public class Animated {
         if (item.getColor() != -1)
             comm.sendRepaint(item.getWurmId(), (byte) WurmColor.getColorRed(item.getColor()), (byte) WurmColor.getColorGreen(item.getColor()), (byte) WurmColor.getColorBlue(item.getColor()), (byte) -1, (byte) 0);
 
+        if (item.isLight()) {
+            int lightStrength;
+            if (item.color != -1) {
+                lightStrength = Math.max(WurmColor.getColorRed(item.color), WurmColor.getColorGreen(item.color));
+                lightStrength = Math.max(1, Math.max(lightStrength, WurmColor.getColorBlue(item.color)));
+                byte r = (byte) (WurmColor.getColorRed(item.color) * 128 / lightStrength);
+                byte g = (byte) (WurmColor.getColorGreen(item.color) * 128 / lightStrength);
+                byte b = (byte) (WurmColor.getColorBlue(item.color) * 128 / lightStrength);
+                comm.sendAttachEffect(item.getWurmId(), (byte) 0, r, g, b, item.getRadius());
+            } else {
+                comm.sendAttachEffect(item.getWurmId(), (byte) 0, Item.getRLight(80), Item.getGLight(80), Item.getBLight(80), item.getRadius());
+            }
+        }
+
         int sz = (int) (Math.min(Hooks.sizeMod(item) * 64f, 255f));
 
         comm.sendResize(item.getWurmId(), (byte) sz, (byte) sz, (byte) sz);
